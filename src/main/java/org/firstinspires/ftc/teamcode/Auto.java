cd4c2457a42b2d44cecf17d5c688bd333e688ba9
sahgals <shreysahgal@gmail.com>
2017-01-28 17:07:08 -0500

    autonomous is fixed
    the app is no longer stuck in a loop.

--- src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -11,86 +11,43 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "Autonomous", group = "Autonomous")
-public class Auto extends OpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    DcMotor backRight;
-    DcMotor backLeft;
-    @Override
-    public void init() {
-		
-    }
-
-    /*
-       * Code to run when the op mode is first enabled goes here
-       * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
-       */
-    @Override
-    public void init_loop() {
-
-      
-
-
-
-
-    }
-    public void wait(int time) {
-        try{
-            Thread.sleep(time*100);
-        }catch(InterruptedException e) {
-            e.printStackTrace();
+ public void wait(int time) {
+        try {
+            Thread.sleep(time * 100);
+        }
+        catch(InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 
-    /*
-     * This method will be called ONCE when start is pressed
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-     */
+    public void drive(double powerRight, double powerLeft, long time) { //moves robot; parameters are relatively self explanatory
 
-    public void moveForward(double pos1, int newTime) {
-        while(runtime.seconds() <= (runtime.seconds() + newTime)) {
-            backRight.setPower(pos1);
-            backLeft.setPower(pos1);
+        long now = System.currentTimeMillis();
+        while(true)
+        {
+            right.setPower(powerRight); left.setPower(powerLeft); //sets power
+
+            if(System.currentTimeMillis() >= now + time)
+            {
+                break;
+            }
         }
-        runtime.reset();
+        right.setPower(0);
+        left.setPower(0);
     }
+
+    public void init(){}public void loop(){}
 
     @Override
     public void start() {
-        loop();
-       
+
+        left = hardwareMap.dcMotor.get("backLeft");
+        right = hardwareMap.dcMotor.get("backRight");
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
-
-
-
-
-    }
-
-    //public void drive(long time, int pos1, int pos2) {
-
-        //if(currentTime - System.currentTimeMillis)
-
-
-
-    /*
-     * This method will be called repeatedly in a loop
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-     */
-    @Override
-    public void loop() {
-
-
-
-
-
-        wait(15);
-
-        moveForward(50, 3);
-    }
-
-    public void stopRobot() {
-        backRight.setPower(0);
-        backLeft.setPower(0);
+        drive(1, 0.5, 1600); // moves to center vortex and knock ball off
+        drive(-1, -1, 500); // moves backward slightly
+        drive(-0.1, 0.1,  8500); // tank turns to beacons
     }
 }
